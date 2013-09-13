7c722a0f48519329f7d078bf2f253c4cb7a97ee0
Forest Johnson <johnsonf@beloit.edu>
2013-09-12 23:47:17 -0500

    commit dat

--- Threads Lab 1/Threads Lab 1/src/edu/beloit/csci335/Lab1Main.java
+++ Threads Lab 1/Threads Lab 1/src/edu/beloit/csci335/Lab1Main.java
@@ -1,28 +1,67 @@
 package edu.beloit.csci335;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Lab1Main {
 
-	/**
-	 * @param args
-	 */
+	private Lab1Thread[] threads;
+	
 	public static void main(String[] args) {
 		
-		// On the first day, god created PlanetA
-		makeThread(args[0], 0.01f, 0.001f, 0.5f); 
+		Lab1Main instance = new Lab1Main();
 		
-		// On the second day, god created PlanetB
-		makeThread(args[1], 0.1f, 0.001f, 0.5f);
+		if(args.length != 3) {
+			System.out.println("ERROR: This program requires three arguements. Exiting now.");
+			return;
+		}
 		
-		// On the third day, god created PlanetC
-		makeThread(args[2], 0.01f, 0.0001f, 0.5f);
-
-		System.out.println("main has ended.");
+		instance.threadDemonstration(args[0], args[1], args[2]);
 	}
 	
-	private static void makeThread(String threadName, float deathFactor, float deathFactorOfSquare, float birthFactor) {
+	public void threadDemonstration(String name1, String name2, String name3) {
+
+		threads = new Lab1Thread[3];
+		
+		threads[0] = makeThread(name1, 0.01f, 0.001f,  0.5f); 
+		threads[1] = makeThread(name2, 0.9f,  0.001f,  0.5f);
+		threads[2] = makeThread(name3, 0.01f, 0.0001f, 0.5f);
+		
+		for(Lab1Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		
+		int maxPopulationIndex = 0;
+		int maxPopulation = 0;
+		int maxLifetimeIndex = 0;
+		int maxLifetime = 0;
+		int i = 0;
+		for(Lab1Thread thread : threads) {
+			if(maxPopulation < thread.getMaxPopulation()) {
+				maxPopulation = thread.getMaxPopulation();
+				maxPopulationIndex = i;
+			}
+			if(maxLifetime < thread.getLifetime()) {
+				maxLifetime = thread.getLifetime();
+				maxLifetimeIndex = i;
+			}
+			i++;
+		}
+		
+		System.out.println("The longest lived planet was " + threads[maxLifetimeIndex].getName() + ", lasting " + maxLifetime + " iterations.");
+		System.out.println("The planet with the largest peak population was " + threads[maxPopulationIndex].getName() + ", with a population of " + maxPopulation + ".");
+	}
+	
+	private Lab1Thread makeThread(String threadName, float deathFactor, float deathFactorOfSquare, float birthFactor) {
 		Lab1Thread thread = new Lab1Thread(threadName, deathFactor, deathFactorOfSquare, birthFactor);
 		System.out.println("Thread is named " + thread.getName());
 		thread.start();
+		return thread;
 	}
 
 }
--- Threads Lab 1/Threads Lab 1/src/edu/beloit/csci335/Lab1Thread.java
+++ Threads Lab 1/Threads Lab 1/src/edu/beloit/csci335/Lab1Thread.java
@@ -2,17 +2,27 @@ package edu.beloit.csci335;
 
 public class Lab1Thread extends Thread {
 	
-	public static final int INTERVAL_MS = 500;
+	public static final int INTERVAL_MS = 200;
 	
 	private float deathFactor;
 	private float deathFactorOfSquare;
 	private float birthFactor;
+	
+	private int maxPopulation;
+	private int lifetime;
 	
 	public Lab1Thread (String name, float deathFactor, float deathFactorOfSquare, float birthFactor) {
 		super(name);
 		this.deathFactor = deathFactor;
 		this.deathFactorOfSquare = deathFactorOfSquare;
 		this.birthFactor = birthFactor;
+	}
+	
+	public int getMaxPopulation () {
+		return maxPopulation;
+	}
+	public int getLifetime () {
+		return lifetime;
 	}
 	
 	public void run () {
@@ -25,6 +35,10 @@ public class Lab1Thread extends Thread {
 			
 			previousPopulation = population;
 			population = Math.max(0, population + births - deaths);
+			if(maxPopulation < population) {
+				maxPopulation = population;
+			}
+			lifetime++;
 			
 			System.out.println(Thread.currentThread().getName() + ": births: " + births + " deaths: " + deaths + " population: " + population);
 			
@@ -34,7 +48,7 @@ public class Lab1Thread extends Thread {
 				return;
 			}
 		}
-		System.out.println("Radio Transmissions from " + Thread.currentThread().getName() + " have ceased");
+		System.out.println("Radio Transmissions from " + Thread.currentThread().getName() + " have ceased after " + lifetime + " iterations.");
 	}
 
 }
